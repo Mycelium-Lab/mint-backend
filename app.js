c872df14bd37792e165371650b722899dba63c04
Danny <daniil.puslis@yandex.ru>
2021-07-29 07:50:02 -0700

    Added pool events subscription. Added telegram bot. Moved some logic to separate modules.

--- app.js
+++ app.js
@@ -1,110 +1,118 @@
-import fetch from 'node-fetch';
-import { MongoClient } from 'mongodb'
-import { ApolloClient, HttpLink, gql } from '@apollo/client/core/core.cjs.js';
-import { InMemoryCache } from '@apollo/client/cache/cache.cjs.js';
+import db from './db.js';
+import { Telegraf } from 'telegraf';
+import Web3 from "web3";
+import fs from 'fs'
 
-const url = 'mongodb://localhost:27017';
-const mongoClient = new MongoClient(url);
-const dbName = 'mint-table';
-const collectionName = 'liquidity-providers';
-
-const connectToDB = async (data) => {
-    await mongoClient.connect();
-    console.log('Connected successfully to server');
-    const db = mongoClient.db(dbName);
-    const collection = db.collection(collectionName);
-    const insertResult = await collection.insertMany(data);
-}
-
-const mintsQuery = gql`
-query GetMint($timestamp: Int!, $amount: String!) {
-  mints(first:1000, where: {amountUSD_gt: $amount, timestamp_gt: $timestamp}, orderBy: origin, orderDirection: desc) {
-    timestamp
-    origin
-    amountUSD
-  token0{
-    symbol
-  }
-  token1{
-    symbol
-  }
-    transaction {
-      burns {
-        amountUSD
-        timestamp
-        token0{
-          symbol
+const bot = new Telegraf(process.env.BOT_TOKEN)
+// Стартовое сообщение
+bot.start((ctx) => {
+    ctx.reply('Send /subscribe to get notifications about uniswap top liquidity providers actions. Send /address to get USD liquidity amount of specific address.')
+})
+// При получении /subscribe ищет по username в БД. Если не находит, то добавляет. 
+bot.command('subscribe', async (ctx) => {
+    try{
+        let subscriber = await db.findOne('telegram-bot-subscribers', 'subscribers', {username: ctx.message.from.username});
+        if(subscriber){
+            ctx.reply('Already sunbscribed!');
+        }else{
+            await db.storeToDB('telegram-bot-subscribers', 'subscribers', [{username: ctx.message.from.username, id: ctx.message.from.id, first_name: ctx.message.from.first_name}]);
+            ctx.reply('Subscribed!');
         }
-        token1{
-          symbol
+    }catch(err){
+        ctx.reply('Error when dealing with subscribtion!');
+        console.log(err);
+    }
+})
+// При получении /address ищет в БД документ по адресу. Если находит, то отправляет количество ликвидности.
+bot.command('address', async (ctx) => {
+    try{
+        let address = ctx.message.text.match(/\/address (0x[a-fA-F0-9]{40})/);
+        if(address){
+            let liquidityProvider = await db.findOne('mint-table', 'liquidity-providers', {address: address[1].toLowerCase()});
+            if(liquidityProvider){
+                ctx.reply(`${address[1].toLowerCase()} address has ${Math.floor(liquidityProvider.totalAmount)}$ liquidity!`);
+            }
+            else
+                ctx.reply('No info about this address!')
+        }else{
+            ctx.reply('Error: Invalid address!');
         }
-      }
+    }catch(err)
+    {
+        ctx.reply('Error: Problem with getting value from database.');
+        console.log(err);
+    }
+})
+// TODO: unsubscribe
+
+bot.launch()
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+const contractJson = fs.readFileSync('./abi/IUniswapV3PoolEvents.json');
+const poolEventsAbi = JSON.parse(contractJson);
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/' + process.env.INFURA_API_KEY));
+const currentBlockNumber = await web3.eth.getBlockNumber();
+
+// Сделать запрос к БД и сохранить в массив адреса пулов без дубликатов 
+const poolsQuery = await db.find('mint-table', 'liquidity-providers', {}, {projection: {address: 1, pools: 1, _id: 0}})
+
+let pools = []
+for (let query of poolsQuery) {
+  for (let pool of query.pools){
+    if(!pools.includes(pool)){
+      pools.push(pool)
     }
   }
 }
-`
-const noDup = (data) => { // форматирует запрос их массива событий mint в объект с id кошелька в качетсве ключа
-    //console.log(data.mints);          // Если хочешь сохранить свою психику даже не пытайся разобраться
-    let newObject = {};
-    data.mints.map(element => {
-      if (!(element.origin in newObject)) {
-        newObject[element.origin] = {totalAmount:parseFloat(0),data:[], active:0, length:0};
-      }
-      newObject[element.origin].totalAmount = newObject[element.origin].totalAmount + parseFloat(element.amountUSD); 
-      newObject[element.origin].active +=1;
-      newObject[element.origin].length +=1;
-  
-      newObject[element.origin].data.push({ // я предупреждал
-        date:element.timestamp,
-        token0: element.token0.symbol,
-        token1: element.token1.symbol,
-        amountUSD: element.amountUSD,
-        flag: 1
-      })
-  
-      for (let burn of element.transaction.burns) {
-        //console.log(element.to);
-        newObject[element.origin].length +=1;
-        if ((parseFloat(element.amountUSD) - parseFloat(burn.amountUSD)) < parseFloat(1000)) {
-          newObject[element.origin].active -= 1;
-          //console.log(parseFloat(element.amountUSD) - parseFloat(burn.amountUSD));
-        }
-        
-        newObject[element.origin].data.push({
-        date: burn.timestamp,
-        token0: burn.token0.symbol,
-        token1: burn.token1.symbol,
-        amountUSD: burn.amountUSD,
-        flag: 0
-      })
-      }
-    });
-    return newObject;
-  }
-const today = new Date();
-const yearAgo = new Date(today.getFullYear()-1, today.getMonth(), today.getDate()).getTime() / 1000;
-const mintsParameters = {
-    timestamp: parseInt(yearAgo),
-    amount: "1000000"
-}
+let poolContracts = []
+pools.forEach((pool) => {
+  poolContracts.push(new web3.eth.Contract(poolEventsAbi, pool))
+});
 
-const link = new HttpLink({
-  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
-  fetch
-});
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
-});
-const { loading, error, data, refetch, networkStatus }= await client.query({ query: mintsQuery , variables: mintsParameters});
-let mintsObject = noDup(data);
-const mintsToArray = Object.keys(mintsObject).map((addressKey) => {
-    return {
-        address: addressKey, 
-        totalAmount: mintsObject[addressKey].totalAmount, 
-        active: mintsObject[addressKey].active, 
-        length: mintsObject[addressKey].length, 
-        data: mintsObject[addressKey].data
+// Функция посылает всем подписчикам бота сообщение о новом эвенте. 
+const eventHandler = async (event) => {
+    try{
+        let subscribers = await db.find('telegram-bot-subscribers', 'subscribers', {});
+        if(subscribers.length > 0){
+            subscribers.forEach((subscriber) => {
+                bot.telegram.sendMessage(subscriber.id, `${event.event} event. Owner: ${event.returnValues.owner}, amount: ${event.returnValues.amount}`);
+            })
+        }
+    }catch(err){
+        console.log(err);
+    }
+}
+// Подписаться на эвенты всех пулов из массива
+poolContracts.forEach((pool, index) => {
+    let options = {
+        filter: {
+            owner: poolsQuery.map((query) => {return query.address}) // Отфильтровать эвенты по адресам из БД
+        },
+        fromBlock: currentBlockNumber
     };
-});
-await connectToDB(mintsToArray);
+    pool.events.Mint(options)
+    .on("connected", function(subscriptionId){
+        console.log(`${index + 1}. Connected to ${pool.options.address} MINT events. Subscription ID: ${subscriptionId}`);
+    })
+    .on('data', async (event) => {
+       await eventHandler(event);
+    })
+    pool.events.Burn(options)
+    .on("connected", function(subscriptionId){
+        console.log(`Connected to ${pool.options.address} BURN events. Subscription ID: ${subscriptionId}`);
+    })
+    .on('data', async (event) =>{
+        await eventHandler(event);
+    })
+})
+
+
+
+// UNISWAP GRAPHQL REQUEST + STORE IN DB
+
+// import utils from './utils.js'
+// import makeQuery from './uniswap.js'
+// const { loading, error, data, refetch, networkStatus } = await makeQuery();
+// const mintsToArray = utils.converMintsToAddressArray(data);
+// await db.storeToDB(mintsToArray);
---
+++ db.js
@@ -0,0 +1,42 @@
+
+import { MongoClient } from 'mongodb'
+const url = 'mongodb://localhost:27017';
+const mongoClient = new MongoClient(url);
+
+const storeToDB = async (dbName, collectionName, data) => {
+    try{
+        await mongoClient.connect();
+        const db = mongoClient.db(dbName);
+        const collection = db.collection(collectionName);
+        await collection.insertMany(data);
+    }finally {
+        await mongoClient.close();
+    }
+}
+const find = async (dbName, collectionName,request, options = {}) => {
+    try{
+        await mongoClient.connect();
+        const db = mongoClient.db(dbName);
+        const collection = db.collection(collectionName);
+        const findResult = await collection.find(request, options).toArray();
+        return findResult;
+    }finally {
+        await mongoClient.close();
+    }
+}
+const findOne = async (dbName, collectionName,request, options = {}) => {
+    try{
+        await mongoClient.connect();
+        const db = mongoClient.db(dbName);
+        const collection = db.collection(collectionName);
+        const findeOneResult = await collection.findOne(request, options);
+        return findeOneResult;
+    }finally {
+        await mongoClient.close();
+    }
+}
+export default {
+    storeToDB: storeToDB,
+    find: find,
+    findOne: findOne
+}
